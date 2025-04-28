ae2477e58de6259435579077044421cf335326dd
Raul Moreno <51415413+morenoraul@users.noreply.github.com>
2025-04-27 22:26:32 -0300

    Create actividad_1_1.js

---
+++ Proyecto Integrador I/actividad_1_1.js
@@ -0,0 +1,5 @@
+// Declaración de las variables
+let nombre = "Raúl Moreno";
+let edad = 43;
+// Imprimir el mensaje en la consola
+console.log("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
